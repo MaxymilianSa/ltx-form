fe036461ad80523aedee89cb9e04725a50bb4b49
Maksymilian Sapa <maxsapa@gmail.com>
2025-02-14 11:24:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Install project
+
+1. Clone repository
+2. Run `yarn` to install project dependencies.
+3. Run `yarn dev` to run dev server or `yarn build` for production purposes.
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>Welcome</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
